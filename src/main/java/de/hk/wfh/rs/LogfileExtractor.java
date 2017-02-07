8a1cf174be1008d9e32b167d650e26a89df4c057
Herbert Paul Köglsperger <herbkoeg@gmx.de>
2017-02-07 16:43:30 +0100

    pattern + filename added

--- src/main/java/de/hk/wfh/rs/LogfileExtractor.java
+++ src/main/java/de/hk/wfh/rs/LogfileExtractor.java
@@ -11,7 +11,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-@Path("/")
+@Path("/herbert/")
 public class LogfileExtractor {
     private Logger logger;
 
@@ -21,39 +21,60 @@ public class LogfileExtractor {
 
     @GET
     @Path("/json")
-    @Produces({ "application/json" })
-    public String getHelloWorldJSON() {
-//        http://localhost:8080/wildflyhealth/rest/json
-        return "hallo";
+    @Produces({"application/json"})
+    public String getHelloWorldJSON(
+            @QueryParam("requestId") String requestId,
+            @QueryParam("pattern") String pattern,
+            @QueryParam("logfile") String logfile) throws Exception {
+//   http://localhost:8080/wildflyhealth/rest/json
+//   http://localhost:8080/wildflyhealth/rest/herbert/json?logfile=server.log
+
+        //   String baseDir = System.getProperty("user.dir");
+
+        String retVal = getFileContent(logfile,pattern);
+
+        writeSomeLog();
+        return retVal;
     }
 
     private void writeSomeLog() {
 
-        for (int i= 0 ; i< 10; i++) {
+        for (int i = 0; i < 10; i++) {
             logger.info("hallo " + i);
         }
     }
 
 
-    private String getFileContent(String filename) throws IOException {
+    private String getFileContent(String filename, String pattern) throws IOException {
         StringBuffer sb = new StringBuffer();
         BufferedReader br = null;
         FileReader fr = null;
+
+        String logDir = System.getProperty("user.dir") + "/../standalone/log/";
+
         //String filename = "/Users/palmherby/Entwickeln/jBoss/wildfly-10.1.0.Final/standalone/log/server.log";
+//        http://localhost:8080/wildflyhealth/rest/herbert/json?logfile=server.log&pattern=org.jboss
 
-        fr = new FileReader(filename);
-        br = new BufferedReader(fr);
+        String fileNameAbsPath = logDir + filename;
 
+        System.out.println(fileNameAbsPath);
+
+        fr = new FileReader(fileNameAbsPath);
         File file = new File(filename);
-        System.err.println("filesize: " + file.length() );
-
+        System.err.println("filesize: " + file.length());
 
         String sCurrentLine;
-        br = new BufferedReader(new FileReader(filename));
+        br = new BufferedReader(new FileReader(fileNameAbsPath));
 
         while ((sCurrentLine = br.readLine()) != null) {
-            //        if(sCurrentLine.contains("org.jboss.as.server.deployment"))
-            sb.append(sCurrentLine).append("\n");
+            if(pattern != null) {
+                if (sCurrentLine.contains(pattern)) {
+                    sb.append(sCurrentLine).append("\n");
+                }
+            }
+            else {
+                sb.append(sCurrentLine).append("\n");
+            }
         }
         return sb.toString();
     }
