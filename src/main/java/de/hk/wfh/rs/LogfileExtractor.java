c9e510a5530aa68783c5b099541c966495f64806
Herbert Paul Köglsperger <herbkoeg@gmx.de>
2017-02-19 19:44:57 +0100

    pattern + filename added

--- src/main/java/de/hk/wfh/rs/LogfileExtractor.java
+++ src/main/java/de/hk/wfh/rs/LogfileExtractor.java
@@ -1,6 +1,7 @@
 package de.hk.wfh.rs;
 
 import org.apache.log4j.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -10,6 +11,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/herbert/")
 public class LogfileExtractor {
@@ -24,16 +27,31 @@ public class LogfileExtractor {
     @Produces({"application/json"})
     public String getHelloWorldJSON(
             @QueryParam("requestId") String requestId,
-            @QueryParam("pattern") String pattern,
+            @QueryParam("filterJsonList") String filterList,
+            @QueryParam("ignoreJsonList") String ignoreList,
             @QueryParam("logfile") String logfile) throws Exception {
 //   http://localhost:8080/wildflyhealth/rest/json
 //   http://localhost:8080/wildflyhealth/rest/herbert/json?logfile=server.log
 
         //   String baseDir = System.getProperty("user.dir");
+//        see  http://stackoverflow.com/questions/2602043/rest-api-best-practice-how-to-accept-list-of-parameter-values-as-input
 
-        String retVal = getFileContent(logfile,pattern);
+        String retVal = getFileContent(logfile,filterList);
 
-        writeSomeLog();
+//        System.out.println("ignoreList: " + ignoreList.get(0));
+
+//        writeSomeLog();
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<String> myStringList = new ArrayList<>();
+        myStringList.add("herbert");
+        myStringList.add("koegi");
+
+        SomePojo somePojo = new SomePojo();
+        System.out.println(mapper.writeValueAsString(myStringList));
+
+        // ["herbert","koegi"]
+
         return retVal;
     }
 
@@ -44,15 +62,14 @@ public class LogfileExtractor {
         }
     }
 
-
     private String getFileContent(String filename, String pattern) throws IOException {
         StringBuffer sb = new StringBuffer();
         BufferedReader br = null;
         FileReader fr = null;
+        FileReader fr = null;
 
         String logDir = System.getProperty("user.dir") + "/../standalone/log/";
 
-        //String filename = "/Users/palmherby/Entwickeln/jBoss/wildfly-10.1.0.Final/standalone/log/server.log";
 //        http://localhost:8080/wildflyhealth/rest/herbert/json?logfile=server.log&pattern=org.jboss
 
         String fileNameAbsPath = logDir + filename;
---
+++ src/main/java/de/hk/wfh/rs/SomePojo.java
@@ -0,0 +1,37 @@
+package de.hk.wfh.rs;
+
+/**
+ * Created by palmherby on 08.02.17.
+ */
+public class SomePojo {
+    private int a;
+    private String b1;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getB1() {
+        return b1;
+    }
+
+    public void setB1(String b1) {
+        this.b1 = b1;
+    }
+
+    public String getB2() {
+        return b2;
+    }
+
+    public void setB2(String b2) {
+        this.b2 = b2;
+    }
+
+    private String b2;
+
+
+}
